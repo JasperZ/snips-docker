f3a3347571047cc133bc4ba96f55408106e6c8e1
JasperZ <jasper.android@gmail.com>
2019-02-26 16:22:44 +0100

    - created build script for docker images

---
+++ build_images.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+SNIPS_VERSION="0.61.1"
+
+# build snips base
+cd base
+docker build -t $DOCKER_USERNAME/snips-base:$SNIPS_VERSION --build-arg snips_version=$SNIPS_VERSION .
+cd ..
+
+# build snips dialogue
+cd dialogue
+docker build -t $DOCKER_USERNAME/snips-dialogue:$SNIPS_VERSION --build-arg snips_version=$SNIPS_VERSION .
+cd ..
+
+# build snips hotword
+cd hotword
+docker build -t $DOCKER_USERNAME/snips-hotword:$SNIPS_VERSION --build-arg snips_version=$SNIPS_VERSION .
+cd ..
+
+# build snips nlu
+cd nlu
+docker build -t $DOCKER_USERNAME/snips-nlu:$SNIPS_VERSION --build-arg snips_version=$SNIPS_VERSION .
+cd ..
+
+# build snips tts
+cd tts
+docker build -t $DOCKER_USERNAME/snips-tts:$SNIPS_VERSION --build-arg snips_version=$SNIPS_VERSION .
+cd ..
+
+# login to docker hub
+if [[ -z "${CI}" ]]; then
+  docker login
+else
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+fi
+
+# upload images
+docker images
+docker push $DOCKER_USERNAME/snips-dialogue:$SNIPS_VERSION
+docker push $DOCKER_USERNAME/snips-hotword:$SNIPS_VERSION
+docker push $DOCKER_USERNAME/snips-nlu:$SNIPS_VERSION
+docker push $DOCKER_USERNAME/snips-tts:$SNIPS_VERSION
